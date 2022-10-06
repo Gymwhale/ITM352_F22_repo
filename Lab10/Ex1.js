f4917ad8a9ba09faeef718f621807b087e9ff4d8
Gymwhale <alohaxjim@gmail.com>
2022-10-06 11:49:18 -1000

    Lab10

---
+++ Lab10/Ex1.js
@@ -0,0 +1,10 @@
+var year = 2002
+step1 = 02 //select last 2 digits of year (02)
+step2 = 2 //diving step1 by 4
+step3 = step1 + step2; //add your answer to step1
+step6 = step3 + 6; 
+step7 = step6 + 8;
+step9 = step7 - 1;
+step10 = 17%7;
+
+console.log(step10);
