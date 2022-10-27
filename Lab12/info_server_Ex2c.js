b5aa09b89b250982cc0a6b64921a55ca565171ce
Gymwhale <alohaxjim@gmail.com>
2022-10-27 13:34:25 -1000

    Lab12

--- Lab12/info_server_Ex2c.js
+++ Lab12/info_server_Ex2c.js
@@ -1,6 +1,17 @@
 var express = require('express');
 var app = express();
-app.get('*', function (request, response, next) {
-    response.send(request.method + ' to path ' + request.path + request.query.toString());
+app.use(express.urlencoded({ extended: true }));
+app.all('*', function (request, response, next) {
+    console.log(request.method + ' to path ' + request.path);
+    next();
 });
+app.post('/process_form', function (request, response, next) {
+    console.log(request.body);
+    var q = request.body['quantity0'];
+    if (typeof q != 'undefined') {
+        // response.send(`Thank you for purchasing ${q} things!`);
+    } 
+    response.redirect("./invoice.html?" );
+});
+app.use(express.static(__dirname + '/public'));
 app.listen(8080, () => console.log(`listening on port 8080`)); // note the use of an anonymous function here to do a callback
