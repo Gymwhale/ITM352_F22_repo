39b5db82d331c3d10d88e23d74218686c87b88fb
Gymwhale <alohaxjim@gmail.com>
2022-10-31 21:22:36 -1000

    A1 0%

--- Baja_Jimwell_Assignment1/products_data.js
+++ Baja_Jimwell_Assignment1/products_data.js
@@ -1,39 +1,45 @@
 var products = [
     {
-    //Prod 1
-    brand: "Long Sleeve",
-    price: 15.00,
-    image: "BEST_Long_Sleeve.png",
+        //Item 1
+        brand: "Long Sleeve",
+        price: 15.00,
+        image: "BEST_Long_Sleeve.png",
+        stock: 4
     },
     {
-    //Prod 2
-    brand: "Padfolio",
-    price: 10.00,
-    image: "Padfolio.jpg",
+        //Item 2
+        brand: "Padfolio",
+        price: 10.00,
+        image: "Padfolio.jpg",
+        stock: 10
     },
     {
-    //Prod 3
+        //Item 3
         brand: "BESTies Sticker",
         price: 3.00,
         image: "BESTies_Sticker.png",
+        stock: 25
     },
     {
-    //Prod 4
+        //Item 4
         brand: "BEST Shield Sticker",
         price: 2.00,
         image: "BEST_Shield_Sticker.png",
+        stock: 20
     },
     {
-    //Prod 5
-    brand: "Black Dad Hat",
-    price: 20.00,
-    image: "Black_Dad_Hat.jpg",
+        //Item 5
+        brand: "Black Dad Hat",
+        price: 20.00,
+        image: "Black_Dad_Hat.jpg",
+        stock: 12
     },
     {
-    //Prod 2
-    brand: "Stone Dad Hat",
-    price: 25.00,
-    image: "Stone_Dad_Hat.jpg",
+        //Item 6
+        brand: "Stone Dad Hat",
+        price: 25.00,
+        image: "Stone_Dad_Hat.jpg",
+        stock: 15
     }
 ]
 
